bff68e3aa2092017eac45ec74f43e4ff70a098fd
phuocnam213 <vuphuocnam2013@gmail.com>
2025-07-20 12:30:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function sayHi() {
   alert("bruh idk");
   window.location.href = "https://en.wikipedia.org/wiki/2025_Pacific_typhoon_season";
 }
+
+const today = new Date();
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = today.toLocaleDateString('en-US', options);
+document.getElementById("date").textContent = "Today is " + formattedDate;
