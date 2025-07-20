669c0b59b3f867b485434873bf167f2aefff316f
phuocnam213 <vuphuocnam2013@gmail.com>
2025-07-20 11:16:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function sayHi() {
   alert("bruh idk");
-  window.location.href = "https://en.wikipedia.org/wiki/Typhoon_Danas_(2025)";
+  window.location.href = "https://en.wikipedia.org/wiki/2025_Pacific_typhoon_season";
 }
